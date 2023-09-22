f34b6d5d66b17b6e361e16988590d0fc7f992009
Muftiat Bakare Abdullahi <105362326+MUFTIATBAKARE@users.noreply.github.com>
2023-09-22 12:04:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./pages/Auth";
 import Gallery from "./pages/Gallery";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [userId, setUserId] = useState(null);
-  const handleUserId = (uid) => {
-    console.log(uid);
-    setUserId(uid);
-  };
-
-  useEffect(() => {
-    console.log("Id:", userId);
-  }, [userId]);
-
   return (
     <Routes>
       <Route path="/" element={<Auth id={handleUserId} />} />
       <Route
-        path="/gallery/:userId"
-        element={<Gallery authenticated={userId} />}
+        path="/gallery"
+        element={<Gallery />}
       />
     </Routes>
   );
